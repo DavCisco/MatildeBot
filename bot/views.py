9edcd7b7788ffc2cb347f8bf9672a375bed12aa6
Davide Grandis <dgrandis@cisco.com>
2020-04-22 12:16:54 +0200

    fix

--- bot/views.py
+++ bot/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 @csrf_exempt
 def webhook(request):
 
-    print('webhook received\n')
-    print(request.body)
+    print('webhook received, details:')
+    requestdata = json.loads(request.body)
+    print(requestdata)
+    print(requestdata['name'])
+    print('\n')
 
     return HttpResponse('<p>home view of the bot<p>')
 
