581c84ccae9384e20f40a4ea63b2fce875e5f8fc
Davide Grandis <dgrandis@cisco.com>
2020-04-22 22:59:30 +0200

    fix

--- bot/views.py
+++ bot/views.py
@@ -41,7 +41,7 @@ def webhook(request):
                 response = 'status'
             if 'report' in reqText:
                 trialId = re.search(r'\s[0-9]+', reqText)
-                response = 'report for trial ' + str(trialId.group())
+                response = 'report for trial ' + str(trialId.group()).strip()
         else:
             response = 'unauthorized'
 
