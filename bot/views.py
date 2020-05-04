4f7767bb35336f260cd4b4e97a5579bdb33f3ba3
Davide Grandis <dgrandis@cisco.com>
2020-05-04 16:42:58 +0200

    fix

--- bot/views.py
+++ bot/views.py
@@ -199,10 +199,12 @@ def action(person_email, space_id, action, argument):
 
     if action == 'help':
         response = 'these are the commands:\n'
+        response += '```'
         response += ' - list:        detailed report of all trials\n'
         response += ' - status:      summary of all trials\n'
         response += ' - report <id>: report of trial #id\n'
         response += ' - help:        this output'
+        response += '```'
 
     elif action == 'echo':
         response = 'request:\n'
