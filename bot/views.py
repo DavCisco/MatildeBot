f94e48b1b37a1837e42f8b9c6100532575f32a65
Davide Grandis <dgrandis@cisco.com>
2020-05-05 12:06:37 +0200

    fix

--- bot/views.py
+++ bot/views.py
@@ -185,23 +185,26 @@ def authorizedRequest(email, space):
     # for the spaces, the bot must be a member of the Team Space
     api = WebexTeamsAPI(botToken)
 
-    authz_users = ['dgrandis@cisco.com', 'pmanto@rstore.it']
-    spaces = api.rooms.list(teamId=TeamScope)
-    authz_spaces = []
-    for record in spaces:
-        authz_spaces.append(record.id)
-    if email in authz_users and space in authz_spaces:
+    authz_users = ['dgrandis@cisco.com']
+    if email in authz_users:
         return True
     else:
-        return False
+        spaces = api.rooms.list(teamId=TeamScope)
+        authz_spaces = []
+        for record in spaces:
+            authz_spaces.append(record.id)
+        if space in authz_spaces:
+            return True
+        else:
+            return False
 
 
 def action(person_email, space_id, action, argument):
     '''
     # List of actions:
 
-    # - 'help'
-    # - 'list_trials'
+    # - 'help'               -> done
+    # - 'list_trials'        -> done   
     # - 'status'
     # - 'trial_report' with argument = trial_id
 
@@ -245,10 +248,27 @@ def action(person_email, space_id, action, argument):
         api.messages.create(space_id, markdown=message)
 
     elif action == 'list_trials':
-        message = 'creating report...'
+        message = 'checking...'
         api.messages.create(space_id, markdown=message)
         ChannelReport(space_id, person_email)
-    
+
+    elif action == 'trial_report':
+        message = 'checking...'
+        api.messages.create(space_id, markdown=message)
+        OrgReport(space_id, person_email, argument)
+
+    elif action == 'status':
+        mention = '<@personEmail:{}>'.format(person_email)
+        message = "relax, life is good :-)\n*Hint: status is work in progress*"
+        api.messages.create(space_id, markdown=message)
+        OrgReport(space_id, person_email, argument)
+
+    elif action == 'unauthorized':
+        message = '*unauthorized access*'
+        api.messages.create(space_id, markdown=message)
+
+
+
     else:
         return
 
@@ -522,10 +542,217 @@ def ChannelReport(space_id, person_email):
     message  = '{}, {}'.format(mention, response)
     api = WebexTeamsAPI(botToken)
     api.messages.create(space_id, markdown=message, files=[filename])
-
     logger.info('Report posted')
-
-
 
     connection.close()
     logger.debug('ChannelReport: closed connection to the DB')
+
+def OrgReport(space_id, person_email, trial_id):
+
+    def BuildOrgProvReport(id):
+
+        # Used by OrgProvReport and OrgReport
+
+        reportDate = datetime.datetime.now().strftime("%b %d, %Y %H:%M")
+        cursor = connection.cursor()
+        sql = "SELECT name, wxSiteUrl, provisioningStatus, provisioningError FROM companies WHERE id = '%s'" % id
+        cursor.execute(sql)
+        companyrecord = cursor.fetchone()
+
+        companyName = companyrecord[0]
+        companySite = companyrecord[1]
+
+        # retrieves the accounts (excluding the Billing accounts)
+        sql = "SELECT accounts.firstName, accounts.lastName, accounts.email, accounts.role, accounts.provisioningStatus,"
+        sql += " accounts.isAlreadyInUse, accounts.isDomainClaim, accounts.lastError"
+        sql += " FROM accounts INNER JOIN companiesAndAccounts ON accounts.id = companiesAndAccounts.accountId"
+        sql += " WHERE companiesAndAccounts.companyId = '%s' AND accounts.role <> 'billing'" % id
+        sql += " ORDER BY accounts.firstname, accounts.lastname"
+        cursor.execute(sql)
+        records = cursor.fetchall()
+
+        # creates the Excel report
+        if not os.path.exists('reports'):
+            os.makedirs('reports')
+        filename = 'reports/' + datetime.datetime.now().strftime("%Y-%m-%d") + \
+            '_Report_' + str(id) + '.xlsx'
+        workbook = xlsxwriter.Workbook(filename)
+        worksheet = workbook.add_worksheet('Report')
+
+        # Format
+        worksheet.hide_gridlines(2)
+        worksheet.set_zoom(120)
+        # bigrow = workbook.add_format({'font_size': 13, 'font_name': 'Arial'})
+        worksheet.set_column('A:B', 20)
+        worksheet.set_column('C:C', 45)
+        worksheet.set_column('D:E', 15)
+        worksheet.set_column('F:F', 34)
+        worksheet.set_row(0, 25)
+        worksheet.set_row(2, 22)
+        worksheet.set_row(3, 22)
+        worksheet.set_row(4, 22)
+        worksheet.set_row(5, 22)
+
+        worksheet.autofilter('A8:F999999')
+        worksheet.freeze_panes('A9')
+        h1 = workbook.add_format({'align': 'vcenter', 'font_size': 18,
+                                'bold': True, 'font_color': 'purple', 'font_name': 'Arial'})
+        ar = workbook.add_format(
+            {'align': 'right', 'font_name': 'Arial', 'font_size': 13})
+        ar_bold = workbook.add_format(
+            {'align': 'right', 'bold': True, 'font_name': 'Arial', 'font_size': 13})
+        hl_bd2 = workbook.add_format(
+            {'bg_color': 'yellow', 'bold': True, 'border': 2, 'font_name': 'Arial'})
+        black = workbook.add_format(
+            {'font_color': 'black', 'font_name': 'Arial', 'align': 'left'})
+        black_bd = workbook.add_format(
+            {'font_color': 'black', 'border': 1, 'font_name': 'Arial', 'align': 'left'})
+        # black big (for the provisioning error content, header)
+        black_big = workbook.add_format(
+            {'font_color': 'black', 'font_name': 'Arial', 'align': 'left', 'font_size': 13})
+        # black big, align right (for the trial id, header)
+        black_big_r = workbook.add_format(
+            {'font_color': 'black', 'font_name': 'Arial', 'align': 'left', 'font_size': 13, 'align': 'right'})
+        # black, big, bold (for the summary content, header)
+        black_bb = workbook.add_format(
+            {'font_color': 'black', 'font_name': 'Arial', 'align': 'left', 'font_size': 13, 'bold': True})
+        blue_bd = workbook.add_format(
+            {'font_color': 'blue', 'border': 1, 'font_name': 'Arial', 'align': 'left'})
+        red = workbook.add_format(
+            {'font_color': 'red', 'font_name': 'Arial', 'align': 'left'})
+        red_bd = workbook.add_format(
+            {'font_color': 'red', 'border': 1, 'font_name': 'Arial', 'align': 'left'})
+        red_big = workbook.add_format(
+            {'font_color': 'red', 'font_name': 'Arial', 'align': 'left', 'font_size': 13, 'bold': True})
+        url = workbook.add_format(
+            {'font_name': 'Arial', 'font_size': 13, 'underline': True})
+
+        # title
+        worksheet.write('A1', 'Matilde Provisioning Report', h1)
+        worksheet.write('F1', reportDate, ar)
+
+        # summary
+        worksheet.write('A3', 'Trial id:', ar_bold)
+        worksheet.write('B3', 'Name:', ar_bold)
+        worksheet.write('B4,', 'Site:', ar_bold)
+        worksheet.write('B5', 'Summary:', ar_bold)
+        worksheet.write('B6', 'Provisioning errors:', ar_bold)
+        worksheet.write('A4', id, black_big_r)
+        worksheet.write_url('C3', url='https://admin.webex.com',
+                            string=companyName, tip='Click here', cell_format=url)
+        worksheet.write_url('C4', url='https://' + companySite,
+                            string=companySite, tip='Click here', cell_format=url)
+
+        # details - header
+        worksheet.write('A8', 'First name', hl_bd2)
+        worksheet.write('B8', 'Last name', hl_bd2)
+        worksheet.write('C8', 'Email', hl_bd2)
+        worksheet.write('D8', 'Role', hl_bd2)
+        worksheet.write_comment('D8', 'includes external admins')
+        worksheet.write('E8', 'Status', hl_bd2)
+        worksheet.write('F8', 'Notes', hl_bd2)
+
+        # details - account records and stats
+        row = 8
+        totAdmins = 0
+        provAdmins = 0
+        totUsers = 0
+        provUsers = 0
+
+        for record in records:
+            fname = record[0]
+            lname = record[1]
+            email = record[2]
+            role = record[3].title()
+            prov = record[4]
+            lastError = record[7]
+            # removes the email address from the error message (redundant)
+            lastError = str(lastError).split(':')[0]
+
+            rowFormat = black_bd
+            # counts admins and users and sets the error and the format of the row
+            error = ''
+            if 'admin' in role.lower():
+                role = "Admin"
+                rowFormat = blue_bd
+                totAdmins += 1
+                if prov == 'provisioned':
+                    provAdmins += 1
+                else:
+                    error = lastError
+                    rowFormat = red_bd
+            else:
+                totUsers += 1
+                if prov == 'provisioned':
+                    provUsers += 1
+                else:
+                    error = lastError
+                    rowFormat = red_bd
+
+            worksheet.write(row, 0, fname,        rowFormat)
+            worksheet.write(row, 1, lname,        rowFormat)
+            worksheet.write(row, 2, email,        rowFormat)
+            worksheet.write(row, 3, role,         rowFormat)
+            worksheet.write(row, 4, prov.title(), rowFormat)
+            worksheet.write(row, 5, error,        rowFormat)
+            row += 1
+
+        # creates summary strings
+        stringAdmins = 'admin'
+        if totAdmins != 1:
+            stringAdmins += 's'
+        stringUsers = 'user'
+        if totUsers != 1:
+            stringUsers += 's'
+        admins = str(provAdmins) + '/' + str(totAdmins)
+        users = str(provUsers) + '/' + str(totUsers)
+        perc = int((provAdmins+provUsers)/(totAdmins+totUsers)*100)
+        summary = str('{}% provisioned ({} ' + stringAdmins +
+                    ', {} ' + stringUsers + ')').format(perc, admins, users)
+        numErrors = str(totAdmins-provAdmins+totUsers-provUsers)
+        fmtErrors = red_big
+        if numErrors == '0':
+            numErrors = 'no'
+            fmtErrors = black_big
+        provErrors = numErrors + ' errors'
+        worksheet.write('C5', summary, black_bb)
+        worksheet.write('C6', provErrors, fmtErrors)
+
+        workbook.close()
+        return filename
+
+    global connection
+
+    api = WebexTeamsAPI(botToken)
+
+    try:
+        connection = pymysql.connect(host=DBhost, port=int(DBport), user=DBuser, password=DBpass, db=DBname)
+    except Exception as e:
+        logger.critical('OrgReport: error opening the DB. ' + str(e) + '. Check settings in the .ini file')
+        logger.critical('Terminating')
+        sys.exit()
+    logger.debug('OrgReport: connected to the DB')
+
+    sql  = "SELECT * FROM companies WHERE id= '%s'" % trial_id
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    company = cursor.fetchone()
+    if not company:
+        response = 'invalid trial id {}. Hint: list all trials first'.format(trial_id)
+        mention = '<@personEmail:{}>'.format(person_email)
+        message = '{}, {}'.format(mention, response)
+        api.messages.create(space_id, markdown=message)
+        logger.info('OrgReport: invalid trialId requested')
+
+    else:
+        # Creates the report in the "/reports" folder
+        filename = BuildOrgProvReport(trial_id)
+        # posts the report
+        response = 'find here the report for the requested trial'
+        mention = '<@personEmail:{}>'.format(person_email)
+        message = '{}, {}'.format(mention, response)
+        api.messages.create(space_id, markdown=message, files=[filename])
+        logger.info('Report for trial_id #{} posted'.format(trial_id))
+
+    connection.close()
+    logger.debug('OrgReport: closed connection to the DB')
